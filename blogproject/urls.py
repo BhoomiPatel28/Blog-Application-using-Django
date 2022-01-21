1383ab4f904ba49a6c9b5c8afed563578169c774
BhoomiPatel28 <63141871+BhoomiPatel28@users.noreply.github.com>
2022-01-21 16:41:22 +0530

    Update urls.py

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -1,7 +1,18 @@
+from xml.dom.minidom import Document
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls import url
+from django.views.static import serve
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blogapp.urls')),
+
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
+
+urlpatterns=urlpatterns + static(settings.MEDIA_ROOT, document_root=settings.MEDIA_ROOT)
+ 
